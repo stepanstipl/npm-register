d4fbc15c92724851e1e1ee893e88fcaf59f77c84
Stepan Stipl <stepan@stipl.net>
2016-12-12 12:58:08 +0000

    Remove token requirement when using auth tyoe ssl_header or none

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,9 +3,17 @@
 const user = require('../lib/user')
 
 module.exports = function * (next) {
-  if (this.headers.authorization) {
-    let token = this.headers.authorization.split(' ')[1]
-    this.username = yield user.findByToken(token)
+
+  if (config.auth.type == 'ssl_header') {
+    let userinfo = yield user.parseDn(this.headers[config.auth.ssl.header.toLowerCase()])
+    this.username = userinfo[0]
+  } else if (config.auth.type == 'none') {
+    this.username = "none"
+  } else {
+    if (this.headers.authorization) {
+      let token = this.headers.authorization.split(' ')[1]
+      this.username = yield user.findByToken(token)
+    }
   }
   if (!this.username) this.throw(401)
   yield next
